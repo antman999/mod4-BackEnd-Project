b545803b0bb12560004e4aa6b4c662653e290477
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-07-23 16:39:46 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Pet.destroy_all
+
+User.create!(first_name:'Bob',last_name:'skywalker',bio:'love animals',email:'theForceIstrong@ST.com',pet_preference:'Dog',foster?:false,adopt?:true,age:23,country:'united states',city:'new york', zip:11334,password:'123')
+
+Pet.create!(pet_type:'Dog', breed:'Pitbull',age:2,sex:'Male',country:'United States', img_url:'https://getyourpet.com/wp-content/uploads/2018/12/pitbull-in-houston-yard.jpg', adopted:false, can_adopt?:true, can_foster?:true)
